1b120e5af51e3b18928c6256df7de614e5a940ba
sudosf <sudosprogrammer@gmail.com>
2023-10-21 22:52:56 +0200

    tailwond intro

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Employee from "./components/Employee";
 
 function App() {
-  return (
-    <>
-    
-    </>
-  );
+    return (
+        <>
+            <div className="flex flex-wrap">
+                <Employee
+                    name={"sudo"}
+                    role={"developer BD"}
+                    image={
+                        "https://tigerbeat.com/wp-content/uploads/2018/11/Screen-Shot-2018-11-13-at-9.27.45-AM-960x640.png"
+                    }
+                />
+                <Employee
+                    name={"roses"}
+                    role={"designer"}
+                    image={
+                        "https://tigerbeat.com/wp-content/uploads/2018/11/Screen-Shot-2018-11-13-at-9.27.45-AM-960x640.png"
+                    }
+                />
+                <Employee
+                    name={"roses"}
+                    role={"designer"}
+                    image={
+                        "https://tigerbeat.com/wp-content/uploads/2018/11/Screen-Shot-2018-11-13-at-9.27.45-AM-960x640.png"
+                    }
+                />
+                <Employee
+                    name={"sudo"}
+                    role={"developer FD"}
+                    image={
+                        "https://tigerbeat.com/wp-content/uploads/2018/11/Screen-Shot-2018-11-13-at-9.27.45-AM-960x640.png"
+                    }
+                />
+            </div>
+        </>
+    );
 }
 
 export default App;
