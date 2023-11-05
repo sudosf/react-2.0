ad156c1024c9e8bbbe2d63b4a28cee097a2aa0b3
sudosf <sudosprogrammer@gmail.com>
2023-11-05 23:29:46 +0200

    added edit modal dialog

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Employee from "./components/Employee";
 import React from "react";
 import { v4 as uuidv4 } from "uuid";
--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,10 +1,12 @@
+import EditEmployee from "./EditEmployee";
+
 export default function Employee(props) {
     return (
         <div className="py-8 px-8 m-2 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
             <img
                 className="object-cover h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
                 src={props.image}
-                alt="Woman's Face"
+                alt="profile"
             />
             <div className="text-center space-y-2 sm:text-left">
                 <div className="space-y-0.5">
@@ -13,9 +15,10 @@ export default function Employee(props) {
                     </p>
                     <p className="text-slate-500 font-medium">{props.role}</p>
                 </div>
-                <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                    Message
-                </button>
+
+                <EditEmployee />
+
+
             </div>
         </div>
     );
